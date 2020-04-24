80c6f4a1e703be4f0497f7eb3a4db31eba20a01e
shivenk78 <kumar.shiven15@gmail.com>
2020-04-24 19:30:43 -0400

    Added Main.kt, added some kTor dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,6 +13,12 @@ repositories {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     testCompile("junit", "junit", "4.12")
+
+    val ktorVersion = "1.3.0"
+    implementation("io.ktor:ktor-server-core:$ktorVersion")
+    implementation("io.ktor:ktor-server-netty:$ktorVersion")
+    implementation("io.ktor:ktor-gson:$ktorVersion")
+    implementation("io.ktor:ktor-client-core:$ktorVersion")
 }
 
 configure<JavaPluginConvention> {
