d93bd4dc8cb274d04dc068ce1f9948e28a563c7e
shivenk78 <kumar.shiven15@gmail.com>
2020-04-24 21:56:22 -0400

    Added runBlocking main to request from Places API

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,24 +2,34 @@ package hngry
 
 import io.ktor.client.*
 import io.ktor.client.request.*
+import kotlinx.coroutines.*
 
 private val API_KEY = "AIzaSyAkJ3TBOV2UlqNqmXgZNH6AbrIhWA_6w9Q"
+private val HOME_LAT = 40.425140
+private val HOME_LNG = -74.544760
 
-suspend fun getPlaces(lat: Double, lng: Double, keyword: String) {
+suspend fun getPlaces(lat: Double, lng: Double) : String {
 
     var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?"
-
-    url += makeRequestParam("key", API_KEY);
+    url += "location".makeRequestParam("$lat,$lng")
+    url += "type".makeRequestParam("restaurant")
+    url += "rankby".makeRequestParam("distance")
+    url += "key".makeRequestParam(API_KEY);
 
     val client = HttpClient()
 
-    val firstBytes = client.get<ByteArray>()
+    val request = client.get<String>(url)
+    return request
 }
 
-fun makeRequestParam(name: String, value: String) : String{
-    return "&$name=$value"
+fun String.makeRequestParam(value: String) : String{
+    return "&$this=$value"
 }
 
-fun main() {
+fun main() = runBlocking {
+    println ("Starting request")
 
+    println(getPlaces(HOME_LAT, HOME_LNG))
+
+    println ("End of Request")
 }
